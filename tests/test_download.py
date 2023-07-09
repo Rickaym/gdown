095682cbb55e11377be65697457217fd1d8e488e
Ricky <66440980+Rickaym@users.noreply.github.com>
2023-07-09 13:37:59 +0630

    Add test for the chrome download

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,6 +1,7 @@
 import os
 
 from gdown.download import download
+from gdown.webdriver_utils import init_chrome_driver
 
 
 def test_download():
@@ -9,4 +10,13 @@ def test_download():
 
     # Usage before https://github.com/wkentaro/gdown/pull/32
     assert download(url, output, quiet=False) == output
+    os.remove(output)
+
+
+def test_chrome_download():
+    url = "https://drive.google.com/file/d/1GwjoIy9EE0Efjomce14MeBZQ5h41Gm09/view?usp=sharing"
+    output = "meme.mp4"
+    init_chrome_driver()
+
+    assert download(url, fuzzy=True, quiet=False) == output
     os.remove(output)
